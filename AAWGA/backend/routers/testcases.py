79c41655f5f17d56df8cb95377a16a10077c1638
hakwoo <43233345+leeahakwoo@users.noreply.github.com>
2025-07-24 17:33:14 +0900

    Update testcases.py

--- AAWGA/backend/routers/testcases.py
+++ AAWGA/backend/routers/testcases.py
@@ -1,5 +1,18 @@
 from fastapi import APIRouter
+from typing import List
+from pydantic import BaseModel
+
+class Testcase(BaseModel):
+    id: str
+    requirement_id: str
+    steps: List[str]
+    expected_result: str
+
+class TestcaseDefinition(BaseModel):
+    testcases: List[Testcase]
+
 router = APIRouter()
-@router.post("", response_model=TestcaseDefinitionOutput)  # 등등
-async def create_testcases(...):
-    ...
+
+@router.post("", response_model=TestcaseDefinition, summary="테스트케이스 생성")
+async def create_testcases(defs: TestcaseDefinition):
+    return defs
